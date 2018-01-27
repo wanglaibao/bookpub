fa0ce33bbcf8add72f349ead3bb3f3374560f078
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-01-27 10:32:27 +0800

    update

--- chapter01/src/main/java/com/laibao/bookpub/commandlinerunner/ThirdRunner.java
+++ chapter01/src/main/java/com/laibao/bookpub/commandlinerunner/ThirdRunner.java
@@ -1,5 +1,6 @@
 package com.laibao.bookpub.commandlinerunner;
 
+import com.laibao.bookpub.repository.BookRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,17 @@ import javax.sql.DataSource;
 public class ThirdRunner implements CommandLineRunner{
     private final Log logger = LogFactory.getLog(ThirdRunner.class);
 
+//    @Autowired
+//    @Qualifier("dataSource")            //为什么必须要这样呢？ 不这样的话 有一个奇怪的问题
+//    private DataSource dataSource;
+
     @Autowired
-    @Qualifier("dataSource")            //为什么必须要这样呢？ 不这样的话 有一个奇怪的问题
-    private DataSource dataSource;
+    private BookRepository bookRepository;
 
     @Override
     public void run(String... args) throws Exception {
         logger.info("I am the third CommandLineRunner java class");
-        logger.info("DataSource: "+ dataSource.toString());
+        //logger.info("DataSource: "+ dataSource.toString());
+        logger.info("Number of books: " + bookRepository.count());
     }
 }
